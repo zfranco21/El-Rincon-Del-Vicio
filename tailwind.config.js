24dbeeb588dbe54254f0a3de97ed69fdb18288e7
franco herrera <126223774+zfranco21@users.noreply.github.com>
2024-04-11 02:24:23 -0300

    Pagina de Inicio semiterminada.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js,css}"],
   theme: {
+    screens: {
+
+      'celu': {'min': '100px', 'max': '640px'},
+
+      'tablet': {'min': '640px', 'max': '1024px'},
+      // => @media (min-width: 640px) { ... }
+
+      'pc': {'min': '1000px', 'max': '1280px'},
+      // => @media (min-width: 1024px) { ... }
+
+      'tv': {'min': '1280px', 'max': '2500px'},
+      // => @media (min-width: 1280px) { ... }
+    },
     extend: {},
   },
   plugins: [],
