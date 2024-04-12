9331347fc1351fd1b7023985304deef55876fe59
franco herrera <126223774+zfranco21@users.noreply.github.com>
2024-04-12 14:59:40 -0300

    Hice la funcion de rotacion cada 7 segundos, todavia quedan cosas para mejorar

--- src/Funciones js/rotacionMain.js
+++ src/Funciones js/rotacionMain.js
@@ -1,25 +1,48 @@
-//Franco: Crear una funcionalidad para rotar elementos de la pagina de inicio
-//rotacion cada 5 segundos
+//Franco: Crear una funcionalidad para rotar elementos de la pagina de inicio.
+//elementos a rotar: Video, titulos del juego,Descripcion y url para ver el trailer.
+//rotacion cada 5 segundos.
 
-const inicio = [{ 
-    urlImagen: "https://www.youtube.com/embed/7b74IRV0vBg?si=xxuEC6DF0almOnto",
-    sliderTitulo: "Street Fighter 3",
-    sliderTexto: "Street Fighter Alpha 3 es un juego de lucha lanzado en 1998 conocido por su jugabilidad rápida y frenética, así como por su amplia selección de personajes. Es uno de los mejores juegos de lucha de la era de los 32 bits.",
-},
-{ 
-    urlImagen: "https://www.youtube.com/embed/7b74IRV0vBg?si=xxuEC6DF0almOnto",
-    sliderTitulo: "Resident Evil 2",
-    sliderTexto: "Street Fighter Alpha 3 es un juego de lucha lanzado en 1998 conocido por su jugabilidad rápida y frenética, así como por su amplia selección de personajes. Es uno de los mejores juegos de lucha de la era de los 32 bits.",
-}];
 
+//constante incio que guarda los cambios que deben hacerse en un array
+const inicio = [
+    { 
+        video: "https://www.youtube.com/embed/7b74IRV0vBg?si=xxuEC6DF0almOnto",
+        sliderTitulo: "Street Fighter 3",
+        sliderTexto: "Street Fighter Alpha 3 es un juego de lucha lanzado en 1998 conocido por su jugabilidad rápida y frenética, así como por su amplia selección de personajes. Es uno de los mejores juegos de lucha de la era de los 32 bits.",
+        trailer: "https://www.youtube.com/watch?v=7b74IRV0vBg&t=1055s",
+    },
+    { 
+        video: "https://www.youtube.com/embed/UZNOI3VTWBk?si=wTx2pRakdWUBYKWg",
+        sliderTitulo: "Metal Gear Solid",
+        sliderTexto: "Metal Gear Solid es un videojuego de acción-aventura y sigilo desarrollado por Konami Computer Entertainment. Lanzado originalmente para la PlayStation en 1998, se convirtió en uno de los títulos más influyentes de su generación.",
+        trailer: "https://www.youtube.com/watch?v=UZNOI3VTWBk&t=1s",
+    },
+    { 
+        video: "https://www.youtube.com/embed/A4OuWAvDLBo?si=J9Pyzu9ov1D7mPx7",
+        sliderTitulo: "Medal Of Honor",
+        sliderTexto: "Esta saga se centra en ofrecer al jugador videojuegos de disparos de corte bélico que habitualmente están ambientados durante la Segunda Guerra Mundia.",
+        trailer: "https://www.youtube.com/watch?v=A4OuWAvDLBo",
+    }
+];
+
+//le decimos que arranque desde el elemento 0, osea el primero.
 let currentIndex = 0;
 
+//Creamos la funcion que va a interactuar con los ID de cada elemento en el html.
+//le pasamos las id de los elementos que queremos cambiar
+//accedemos con .src o .textContent al contenido especifico que necesitamos
+//luego le decimos que cambie el contenido ID por lo que esta en nuestra variable inicio
+//y por ultimo currentIndex nos ayuda a pasar los videos 1 x 1 y cuando llegue al ultimo vuelva al primero.
 function cambiar() {
-    document.body.style.backgroundImage = "url('" + inicio[currentIndex].urlImagen + "')";
+    document.getElementById("videoFrame").src = inicio[currentIndex].video;
     document.getElementById("sliderTitulo").textContent = inicio[currentIndex].sliderTitulo;
     document.getElementById("sliderTexto").textContent = inicio[currentIndex].sliderTexto;
+    document.getElementById("trailer").href = inicio[currentIndex].trailer;
     currentIndex = (currentIndex + 1) % inicio.length;
 }
 
-setInterval(cambiar, 5000);
+//usamos setInterval que es un metodo de bucle cada cierto tiempo
+//como tenemos un array, va a ir pasando de un elemento a otro cada 7 segundos, y luego se reinicia el bucle.
+setInterval(cambiar, 7000);
 window.onload = cambiar;
+//Window.onload = cambiar; : Esto le dice al navegador que cuando cargue toda la pagina empiece la funcion cambiar.
