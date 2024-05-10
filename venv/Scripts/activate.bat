7c76d053767f6675f5be5b2c62fc1bd4f4b9da79
DylanDSP10 <141286179+DylanDSP10@users.noreply.github.com>
2024-05-09 22:00:22 -0300

    actualizacion flask

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Franco\OneDrive\Documentos\GitHub\El-Rincon-Del-Vicio\venv
+set VIRTUAL_ENV=C:\Users\PiChuSama\Documents\GitHub\El-Rincon-Del-Vicio\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
